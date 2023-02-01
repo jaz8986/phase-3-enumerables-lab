35d2de5a568d25b27b963d7d4b60901b8302ffcc
Jaskiran Singh <jsingh8986@yahoo.com>
2023-02-01 14:21:12 -0800

    commit tree 4748390776146a7f6e7b8c2cd3a0ab64e948ae7d

--- enumerables.rb
+++ enumerables.rb
@@ -3,22 +3,31 @@ require 'pry'
 # this method returns an array of hashes, which we'll use in the other methods
 def spicy_foods 
   [
-    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
-    { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
-    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 }
+    { name: 'Green Curry', 
+      cuisine: 'Thai', 
+      heat_level: 9 
+    },
+    { name: 'Buffalo Wings', 
+      cuisine: 'American', 
+      heat_level: 3 
+    },
+    { name: 'Mapo Tofu', 
+      cuisine: 'Sichuan', 
+      heat_level: 6 
+    }
   ]
 end
 
 # given an array of spicy foods, **return an array of strings**
 # with the names of each spicy food
 def get_names(spicy_foods)
-  # your code here
+  spicy_foods.map {|food| food[:name]}
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
-  # your code here
+  spicy_foods.select {|food| food[:heat_level] > 5}
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -27,13 +36,15 @@ end
 # HINT: you can use * with a string to produce the correct number of 🌶 emoji. 
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
-  # your code here
+  spicy_foods.map do |food|
+    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{"🌶" * food[:heat_level]}"
+  end
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-  # your code here
+  spicy_foods.filter {|food| food[:cuisine] == cuisine}.uniq
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
